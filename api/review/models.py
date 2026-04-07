8b54b50dae5de2189837385ca0e36e8070866284
felps019 <felipedocs313@gmail.com>
2026-04-07 19:28:08 -0300

    fix: adjusts name of route user

--- api/review/models.py
+++ api/review/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from travelplans.models import TravelPlan
+from django.conf import settings
 
 class Review(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
