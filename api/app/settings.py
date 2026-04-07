38a769b46636f376a4ba6295627b21564f7f4f67
Felipe Kenzo Ogaçawara Sunakozawa <felipe.sunakozawa@gmail.com>
2026-04-07 15:57:27 -0300

    Corrigindo url e adicionando usuarios no settings

--- api/app/settings.py
+++ api/app/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'travelplans',
     'favorites',
+    'usuarios',
 ]
 
 MIDDLEWARE = [
--- api/app/urls.py
+++ api/app/urls.py
@@ -9,10 +9,19 @@ from favorites.views import (
     FavoriteRetrieveUpdateDestroy,
 )
 
+from usuarios.views import (
+    PreferenciaUsuarioViewSet,
+    PreferenciaViagemViewSet,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('travel-plans/', TravelPlanCreateListView.as_view(), name='travel-plan-create-list'),
     path('travel-plans/<int:pk>', TravelPlanRetrieveUpdateDestroy.as_view(), name='travel-plan-detail-view'),
     path('favorites/', FavoriteCreateListView.as_view(), name='favorite-create-list'),
     path('favorites/<int:pk>', FavoriteRetrieveUpdateDestroy.as_view(), name='favorite-detail-view'),
+    path('preferencias-usuario/', PreferenciaUsuarioViewSet.as_view({'get': 'list', 'post': 'create'}), name='preferencia-usuario-list'),
+    path('preferencias-usuario/<int:pk>/', PreferenciaUsuarioViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='preferencia-usuario-detail'),
+    path('preferencias-viagem/', PreferenciaViagemViewSet.as_view({'get': 'list', 'post': 'create'}), name='preferencia-viagem-list'),
+    path('preferencias-viagem/<int:pk>/', PreferenciaViagemViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='preferencia-viagem-detail'),
 ]
