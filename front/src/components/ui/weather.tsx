6dcae8231f4822905eb68541037bba8c4d132cc7
yurichryst <yurichryst@gmail.com>
2026-05-16 23:28:42 -0300

    Att Components

--- front/src/components/ui/weather.tsx
+++ front/src/components/ui/weather.tsx
@@ -2,30 +2,41 @@ import { Sun } from "lucide-react";
 
 export default function Weather() {
   return (
-    <div className="pt-8 flex flex-col border  border-neutral-300 items-center justify-content rounded-2xl">
-      <h2>Clima e previsao do tempo</h2>
-      <div className="py-8 px-8">
+    <div className="py-8 flex flex-col border border-neutral-300 rounded-2xl">
+      <h2 className="px-8">Clima e previsão do tempo</h2>
+      <div className="py-8 px-8 w-full">
         <div className="border border-neutral-300 rounded-2xl">
-          <div className="pt-8 flex flex-row">
-            <Sun ""/>
-
-            <div className="flex flex-col">
-              <h2> 32°C</h2>
-              <p>ensolarado</p>
+          <div className="px-4 py-4 flex flex-row">
+            <Sun className="stroke-yellow-400 w-10 h-10" />
+            <div className="px-3 flex flex-col">
+              <h2 className="text-3xl">32°C</h2>
+              <p>Ensolarado, sem chuva prevista</p>
             </div>
           </div>
-          <div><p>Umidade 74%</p>
-          <p>UV índice 9 — Alto</p>
-          <p>Vento 18 km/h</p></div>
-          <div></div>
-        </div>
-        <h2 className="pt-8">Custo estimados</h2>
-        <div className="pt-8">
-          <div className="border border-neutral-300 rounded-2xl">
-            <p>Passeios e entradas</p>
+          <div className="px-4 py-4">
+            <p>Umidade 74%</p>
+            <p>UV índice 9 — Alto</p>
+            <p>Vento 18 km/h</p>
           </div>
-          <div className="border border-neutral-300 rounded-2xl">
-            <p>Total estimado: R$ 1.200,00</p>
+        </div>
+        <h2 className="pt-8 pb-8 font-semibold">Custos estimados</h2>
+        <div className="p-4 border border-neutral-300 rounded-2xl p-4">
+          <div className="flex justify-between py-1">
+            <p>Passeios e entradas</p>
+            <p className="font-semibold">R$160</p>
+          </div>
+          <div className="flex justify-between py-1">
+            <p>Alimentação</p>
+            <p className="font-semibold">R$225</p>
+          </div>
+          <div className="flex justify-between py-1">
+            <p>Transporte</p>
+            <p className="font-semibold">R$32</p>
+          </div>
+
+          <div className="border-t border-neutral-300 mt-2 pt-2 flex justify-between">
+            <p className="font-semibold">Total estimado</p>
+            <p className="font-semibold">R$420</p>
           </div>
         </div>
       </div>
--- front/src/pages/tests/test.tsx
+++ front/src/pages/tests/test.tsx
@@ -1,11 +1,9 @@
 import Sidebar from "@/components/ui/Sidebar"; 
-import Weather from "@/components/ui/weather";
 
 export default function Test() {
   return (
     <div className="flex gap-8">
       <Sidebar />
-      <Weather />
     </div>
   );
 }
